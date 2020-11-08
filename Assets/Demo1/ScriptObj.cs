a66e8adfe9767748ad16974087a64fad322f08bd
GeorgeManeta <georgemaneta@gmail.com>
2020-11-08 04:21:40 -0300

    Demo1

---
+++ Assets/Demo1/ScriptObj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace demo1
+{
+    //[CreateAssetMenu(menuName = "Demo1/ScriptObj")]
+    public class ScriptObj : ScriptableObject
+    {
+        public void Awake()
+        {
+            Debug.Log($"Awake {name}");
+        }
+
+        public void OnEnable()
+        {
+            Debug.Log($"OnEnable {name}");
+        }
+
+        public void OnDisable()
+        {
+            Debug.Log($"OnDisable {name}");
+        }
+
+        public void OnDestroy()
+        {
+            Debug.Log($"OnDestroy {name}");
+        }
+    }
+}
---
+++ Assets/Demo1/ScriptObjExample.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace demo1 
+{ 
+    public class ScriptObjExample : MonoBehaviour
+    {
+        public ScriptObj reference;
+
+        void Start()
+        {
+            Debug.Log("Creating instance of ScriptObj");
+            ScriptObj newInstance = (ScriptObj) ScriptableObject.CreateInstance(typeof(ScriptObj));
+        }
+    }
+}
